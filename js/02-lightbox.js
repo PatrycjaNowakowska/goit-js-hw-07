0e98ea6c1e4376ed83a134cb712ec689bace190a
PatrycjaNowakowska <poczta.pnowakowska@gmail.com>
2021-09-05 18:41:21 +0200

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryLightbox = document.querySelector(".gallery");
+
+const items = galleryItems
+    .map((galleryItem) =>
+        `<a class="gallery__item" href="${galleryItem.original}">
+        <img
+            class="gallery__image"
+            src="${galleryItem.preview}"
+            alt="${galleryItem.description}"
+        />
+        </a>`
+    )
+    .join("");
+galleryLightbox.insertAdjacentHTML("afterbegin",items);
+console.log(items);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+});
+console.log(lightbox);
